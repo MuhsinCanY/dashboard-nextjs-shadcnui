cf0bdafa1a1f02fd14519110eed1d97c1b97f9b0
MuhsinCanY <mcyilmaz1998@gmail.com>
2024-08-12 14:46:31 +0300

    Add navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import logo from '../public/logo.jpg'
+import Link from 'next/link'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+export const Navbar = () => {
+  return (
+    <div className="bg-primary dark:bg-slate-700 py-2 px-5 flex justify-between">
+      <Link href={'/'}>
+        <Image
+          src={logo}
+          alt="M Logo"
+          width={40}
+          className="rounded-full"
+        ></Image>
+      </Link>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus:outline-none">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>MY</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href={'/profile'}>Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href={'/auth'}>Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
