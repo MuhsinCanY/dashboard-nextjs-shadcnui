f4e4ae8ea681ec4f1ff42b7202997d7f15c2d09c
MuhsinCanY <mcyilmaz1998@gmail.com>
2024-08-22 16:11:24 +0300

    Add DashboardCard component

---
+++ components/dashboard/DashboardCard.tsx
@@ -0,0 +1,27 @@
+import { Card, CardContent } from '@/components/ui/card'
+import { LucideIcon } from 'lucide-react'
+import React from 'react'
+
+interface DashboardCardProps {
+  title: string
+  count: number
+  icon: React.ReactElement<LucideIcon>
+}
+
+export const DashboardCard = ({ title, count, icon }: DashboardCardProps) => {
+  return (
+    <Card className="bg-slate-100 dark:bg-slate-800 p-4 pb-0">
+      <CardContent>
+        <h3 className="text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">
+          {title}
+        </h3>
+        <div className="flex justify-center items-center gap-5">
+          {icon}
+          <p className="text-5xl font-semibold text-slate-500 dark:text-slate-200">
+            {count}
+          </p>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
